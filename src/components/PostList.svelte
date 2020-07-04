6fcf0f0217e50c3d247f1f6074ecea4cae33b17c
Neil Clarke <neil@rockpaper.technology>
2020-07-04 15:13:43 +0200

    [WP1] Update post url to use HTTPs

--- src/components/PostList.svelte
+++ src/components/PostList.svelte
@@ -2,7 +2,7 @@
     import { onMount } from 'svelte';
     let posts = [];
     onMount(async() => {
-        const postsUrl = 'http://sapperpress.rockpaper.technology/wp-json/wp/v2/posts';
+        const postsUrl = 'https://sapperpress.rockpaper.technology/wp-json/wp/v2/posts';
         const response = await fetch(postsUrl);
         const json = await response.json();
         posts = json;
