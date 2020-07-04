b348c3ad9e935f9daeb5b1fca7c00e4f94efcb07
Neil Clarke <neil@rockpaper.technology>
2020-07-04 14:58:38 +0200

    [WP1] Create initial Wordpress integration
    Based on https://watch-learn.com/creating-svelte-sapper-and-wordpress-website/fetching-posts

---
+++ src/components/PostList.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+    let posts = [];
+    onMount(async() => {
+        const postsUrl = 'http://sapperpress.rockpaper.technology/wp-json/wp/v2/posts';
+        const response = await fetch(postsUrl);
+        const json = await response.json();
+        posts = json;
+    })
+</script>
+
+<ul>
+{#each posts as post}
+    <li>{post.title.rendered}</li>
+{/each}
+</ul>
