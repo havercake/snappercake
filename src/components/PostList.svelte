1fed3c62248c8587b95e8aecb48aaea3db7f5419
Neil Clarke <neil@rockpaper.technology>
2020-07-08 19:41:26 +0200

    [WP-3] Add pagination and SASS
    https://watch-learn.com/creating-svelte-sapper-and-wordpress-website/pagination-and-sass-support

--- src/components/PostList.svelte
+++ src/components/PostList.svelte
@@ -1,19 +1,75 @@
+<ul>
+    {#each posts as post}
+        <li><a rel='prefetch' href='articles/{post.slug}'>{post.title.rendered}</a></li>
+    {/each}
+</ul>
+
+<ul class="pagination">
+    {#each pages as page}
+        <li><button class="{ page === currentPage ? 'active' : '' }" on:click="{() => changePage(page)}">{page}</button></li>
+    {/each}
+</ul>
+
 <script>
     import { onMount } from 'svelte';
+
     let posts = [];
+    let posts_per_page = 3;
+    let totalPages = null;
+    let currentPage = 1;
+    let pages = [];
+
+    function createPagesArray(total) {
+        let array = [];
+        for(let i = 1; i <= total; i++){
+            array.push(i);
+        } 
+        return array;
+    }
+
+    function changePage(page) {
+        const postsUrl = `${apiUrl}/wp/v2/posts?per_page=${posts_per_page}&page=${page}`;
+        fetch(postsUrl).then(response => {
+            return response.json();
+        }).then(result => {
+            posts = result;
+            currentPage = page; 
+        });
+    } 
 
     const apiUrl = process.env.SAPPER_APP_API_URL;
 
     onMount(async() => {
-        const postsUrl = `${apiUrl}/wp/v2/posts`;
+        const postsUrl = `${apiUrl}/wp/v2/posts?per_page=${posts_per_page}`;
         const response = await fetch(postsUrl);
-        const json = await response.json();
-        posts = json;
+        posts = await response.json();
+
+        let totalPosts = response.headers.get('X-WP-Total');
+        console.log(`totalPosts=${totalPosts}`);
+        totalPages = response.headers.get('X-WP-TotalPages');
+        console.log(totalPages);
+        pages = createPagesArray(totalPages);     
+        console.log(pages);
+        
     })
 </script>
 
-<ul>
-{#each posts as post}
-    <li><a rel='prefetch' href='articles/{post.slug}'>{post.title.rendered}</a></li>
-{/each}
-</ul>
+<style lang="scss">
+    .pagination {
+        margin: 0;
+        display: flex;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+        li {
+            padding-right: 1em;
+            padding-bottom: 1em;
+            display: flex;
+            align-items: center;
+            
+            button.active {
+                background: dodgerblue;
+                border-color: dodgerblue; 
+            }
+        }
+    }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const config = require('sapper/config/webpack.js');
 const pkg = require('./package.json');
 const sapperEnvironment = require('sapper-environment');
+const sveltePreprocess = require('svelte-preprocess');
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
@@ -10,6 +11,12 @@ const dev = mode === 'development';
 const alias = { svelte: path.resolve('node_modules', 'svelte') };
 const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
 const mainFields = ['svelte', 'module', 'browser', 'main'];
+
+const preprocess = sveltePreprocess({
+	scss:{
+		includePaths:['src']
+	}
+}); 
 
 module.exports = {
 	client: {
@@ -24,6 +31,7 @@ module.exports = {
 						loader: 'svelte-loader',
 						options: {
 							dev,
+							preprocess,
 							hydratable: true,
 							hotReload: false // pending https://github.com/sveltejs/svelte/issues/2377
 						}
@@ -59,7 +67,8 @@ module.exports = {
 						options: {
 							css: false,
 							generate: 'ssr',
-							dev
+							dev,
+							preprocess
 						}
 					}
 				}
